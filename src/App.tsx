9822a5dd7b2f088225b73ec50e10b607e5137c5e
zzj <2461927976@qq.com>
2022-05-20 16:58:53 +0800

    编辑器接入

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref, Ref } from 'vue'
+import MonacoEditor from './components/MonacoEditor'
+import { createUseStyles } from 'vue-jss'
+
+function toJson(data: any) {
+  return JSON.stringify(data, null, 2)
+}
+
+const schema = {
+  type: 'string',
+}
+
+const useStyles = createUseStyles({
+  editor: {
+    minHeight: 400,
+  },
+})
 
 export default defineComponent({
   setup() {
+    const schemaRef: Ref<any> = ref(schema)
+
+    const handleCodeChange = (code: string) => {
+      let schema: any
+      try {
+        schema = JSON.parse(code)
+      } catch (error) {
+        schemaRef.value = schema
+      }
+    }
+
+    const classRef = useStyles()
+
     return () => {
-      return <p>zzj</p>
+      const classes = classRef.value
+      const code = toJson(schemaRef.value)
+      return (
+        <MonacoEditor
+          code={code}
+          onChange={handleCodeChange}
+          title="schema"
+          class={classes.editor}
+        ></MonacoEditor>
+      )
     }
   },
 })
