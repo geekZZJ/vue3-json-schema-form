0e65a6752c6109b8fce3aab43bc0a4fea6eb8ee0
zzj <2461927976@qq.com>
2022-05-26 16:36:40 +0800

    单元测试

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,55 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { shallowMount } from '@vue/test-utils'
+import { defineComponent, h } from 'vue'
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
+const HelloWorld = defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  setup(props) {
+    return () => {
+      return h('div', props.msg)
+    }
+  },
+})
+
+beforeEach(() => {
+  console.log('beforeEach')
+})
+
+afterEach(() => {
+  console.log('afterEach')
+})
+
+beforeAll(() => {
+  console.log('beforeAll')
+})
+
+afterAll(() => {
+  console.log('afterAll')
+})
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', async () => {
+    const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
+    })
+    // setTimeout(() => {
+    //   expect(wrapper.text()).toMatch(msg)
+    //   done()
+    // }, 100)
+    // return new Promise<void>((resolve) => {
+    //   expect(wrapper.text()).toMatch(msg)
+    //   resolve()
+    // })
+    await wrapper.setProps({
+      msg: '123',
+    })
+    expect(wrapper.text()).toMatch('123')
+  })
+
+  it('should work', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
