38ef2cb5d732ea29544bed04eb9a614c55f3f528
zzj <2461927976@qq.com>
2022-05-27 22:41:18 +0800

    ObjectField单元测试完善

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import SchemaForm from './schemaForm'
 import NumberField from './fields/NumberField'
+import StringField from './fields/StringField'
 
 export default SchemaForm
 
-export { NumberField }
+export { NumberField, StringField }
---
+++ tests/unit/ObjectField.spec.ts
@@ -0,0 +1,81 @@
+import { mount } from '@vue/test-utils'
+import JsonSchemaForm, { StringField, NumberField } from '../../lib'
+
+describe('ObjectField', () => {
+  let schema: any
+  beforeEach(() => {
+    schema = {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string',
+        },
+        age: {
+          type: 'number',
+        },
+      },
+    }
+  })
+
+  it('should render property to correct fields', () => {
+    const wrapper = mount(JsonSchemaForm, {
+      props: {
+        schema,
+        value: {},
+        onChange: () => {
+          // console.log(111)
+        },
+      },
+    })
+
+    const strField = wrapper.findComponent(StringField)
+    const numberField = wrapper.findComponent(NumberField)
+
+    expect(strField.exists()).toBeTruthy()
+    expect(numberField.exists()).toBeTruthy()
+  })
+
+  it('should change value when sub fields trigger onChange', async () => {
+    let value: any = {}
+    const wrapper = mount(JsonSchemaForm, {
+      props: {
+        schema,
+        value,
+        onChange: (v: any) => {
+          value = v
+        },
+      },
+    })
+
+    const strField = wrapper.findComponent(StringField)
+    const numberField = wrapper.findComponent(NumberField)
+
+    await strField.props('onChange')('1')
+    await numberField.props('onChange')(1)
+
+    expect(value.name).toEqual('1')
+    expect(value.age).toEqual(1)
+  })
+
+  it('should render property to correct fields', async () => {
+    let value: any = {
+      name: '123',
+    }
+    const wrapper = mount(JsonSchemaForm, {
+      props: {
+        schema,
+        value,
+        onChange: (v: any) => {
+          value = v
+        },
+      },
+    })
+
+    const strField = wrapper.findComponent(StringField)
+    // const numberField = wrapper.findComponent(NumberField)
+    await strField.props('onChange')(undefined)
+
+    expect(value.name).toBeUndefined()
+    // expect(numberField.exists()).toBeTruthy()
+  })
+})
