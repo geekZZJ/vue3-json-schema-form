55befb14f1df72312fb5e13b4648359d1385f040
zzj <2461927976@qq.com>
2022-05-28 00:28:42 +0800

    ArrayFields单元测试补全

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,9 @@
 import SchemaForm from './schemaForm'
 import NumberField from './fields/NumberField'
 import StringField from './fields/StringField'
+import ArrayField from './fields/ArrayField'
+import Selection from './widgets/Selection'
 
 export default SchemaForm
 
-export { NumberField, StringField }
+export { NumberField, StringField, ArrayField, Selection }
---
+++ tests/unit/ArrayField.spec.ts
@@ -0,0 +1,83 @@
+import { mount } from '@vue/test-utils'
+import JsonSchemaForm, {
+  StringField,
+  NumberField,
+  ArrayField,
+  Selection,
+} from '../../lib'
+
+describe('ArrayField', () => {
+  it('should render a list of fields', () => {
+    const wrapper = mount(JsonSchemaForm, {
+      props: {
+        schema: {
+          type: 'array',
+          items: [
+            {
+              type: 'string',
+            },
+            {
+              type: 'number',
+            },
+          ],
+        },
+        value: [],
+        onChange: () => {
+          // console.log(111)
+        },
+      },
+    })
+
+    const arr = wrapper.findComponent(ArrayField)
+    const str = arr.findComponent(StringField)
+    const num = arr.findComponent(NumberField)
+
+    expect(str.exists()).toBeTruthy()
+    expect(num.exists()).toBeTruthy()
+  })
+
+  it('should render single type', () => {
+    const wrapper = mount(JsonSchemaForm, {
+      props: {
+        schema: {
+          type: 'array',
+          items: {
+            type: 'string',
+          },
+        },
+        value: ['1', '2'],
+        onChange: () => {
+          // console.log(111)
+        },
+      },
+    })
+
+    const arr = wrapper.findComponent(ArrayField)
+    const strs = arr.findAllComponents(StringField)
+
+    expect(strs.length).toBe(2)
+    expect(strs[0].props().value).toBe('1')
+  })
+
+  it('should render select type', () => {
+    const wrapper = mount(JsonSchemaForm, {
+      props: {
+        schema: {
+          type: 'array',
+          items: {
+            type: 'string',
+            enum: ['1', '2', '3'],
+          },
+        },
+        value: [],
+        onChange: () => {
+          // console.log(111)
+        },
+      },
+    })
+
+    const select = wrapper.findComponent(Selection)
+
+    expect(select.exists()).toBeTruthy()
+  })
+})
